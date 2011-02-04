ec43baa70bf0a05c3d6b588f1262cdea767a8ae1
Matthew Kitt <mk.kitt@gmail.com>
2011-02-04 16:42:16 -0700

    Start of a basic benchmarking suite.

---
+++ benchmark/stopwatch.js
@@ -0,0 +1,32 @@
+
+var Stopwatch = function () {
+  this.log = null;
+  this.times = null;
+};
+
+Stopwatch.prototype = {
+
+  initialize: function () {
+
+  },
+
+  pad: function (str, len) {
+
+  },
+
+  trace: function (message) {
+
+  },
+
+  time: function (fn) {
+
+  },
+
+  benchmark: function (label, fn) {
+
+  },
+
+  dispose: function () {
+
+  }
+};
