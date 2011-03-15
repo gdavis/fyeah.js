c97d237633f5768dd998409bdca469ef5661750e
Matthew Kitt <mk.kitt@gmail.com>
2011-03-14 22:37:03 -0600

    Temporary build result

---
+++ dist/fyeah.js
@@ -0,0 +1,23 @@
+/*!
+ * fyeah JavaScript Toolkit v0.0.1
+ * http://www.factorylabs.com/
+ *
+ * Copyright 2011, Factory Design Labs
+ * Licensed under the MIT license.
+ * https://github.com/factorylabs/fyeah.js/blob/master/MIT-LICENSE
+ *
+ * Date: Tue Feb 15 09:42:35 2011 -0700
+ */
+(function(window, undefined) {
+  var document = window.document;
+
+
+var fyeah = (function() {
+  // expose fyeah to the global object
+  return fyeah;
+}());
+
+
+  window.fyeah = fyeah;
+})(window);
+
